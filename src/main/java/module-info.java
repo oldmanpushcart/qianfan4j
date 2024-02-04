f4fac35ca615d556ac88673bb4f8d1123f2d448c
oldmanpushcart <oldmanpushcart@gmail.com>
2024-02-05 02:35:01 +0800

    优化module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,7 +5,6 @@ module erniebot4j {
     requires transitive java.desktop;
 
     requires com.fasterxml.jackson.databind;
-    requires com.fasterxml.jackson.annotation;
     requires com.fasterxml.jackson.core;
     requires com.fasterxml.jackson.module.jsonSchema;
 
