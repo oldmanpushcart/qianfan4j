f3b0639de1125985b00ac96c2a7bcd1aecf3d131
oldmanpushcart <oldmanpushcart@gmail.com>
2024-02-11 20:56:43 +0800

    添加注释

--- src/test/java/io/github/ompc/erniebot4j/test/DebugTestCase.java
+++ src/test/java/io/github/ompc/erniebot4j/test/DebugTestCase.java
@@ -1,27 +1,30 @@
 package io.github.ompc.erniebot4j.test;
 
-import io.github.ompc.erniebot4j.chat.ChatModel;
-import io.github.ompc.erniebot4j.chat.ChatRequest;
-import io.github.ompc.erniebot4j.chat.message.Message;
+import io.github.ompc.erniebot4j.plugin.Plugin;
+import io.github.ompc.erniebot4j.plugin.PluginModel;
+import io.github.ompc.erniebot4j.plugin.PluginRequest;
 import org.junit.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class DebugTestCase implements LoadingProperties {
 
     @Test
-    public void test$debug() {
-        final var request = new ChatRequest.Builder()
-                .model(ChatModel.ERNIEBOT_V4)
-                .message(Message.human("""
-                        你可以解读图片链接中的图片吗？
-                        """))
-                //.option(ChatOptions.IS_ENABLE_SEARCH, false)
-                //.option(ChatOptions.IS_ENABLE_CITATION, true)
-                //.function(new EchoFunction())
+    public void test$debug() throws MalformedURLException {
+
+        final var request = new PluginRequest.Builder()
+                .model(new PluginModel("ujwdafb5hz1e4qee"))
+                .plugins(Plugin.KNOWLEDGE_BASE, Plugin.CHAT_OCR)
+                .question("解析这张图片")
+                .imageUrl(new URL("https://erniebot4j-image.bj.bcebos.com/bfa05c91-99ed-419f-bcca-06823f3bcb4b.png"))
                 .build();
-        final var response = client.chat(request)
+
+        final var response = client.plugin(request)
                 .async()
                 .join();
-        System.out.println(response.sentence().content());
+
+        System.out.println(response);
     }
 
 }
