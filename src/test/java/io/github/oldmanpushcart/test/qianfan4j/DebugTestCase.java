69120e2757e55719835535aa46a388cfd2e51abf
oldmanpushcart <oldmanpushcart@gmail.com>
2024-03-10 23:09:22 +0800

    发布1.0.0

--- src/test/java/io/github/oldmanpushcart/test/qianfan4j/DebugTestCase.java
+++ src/test/java/io/github/oldmanpushcart/test/qianfan4j/DebugTestCase.java
@@ -1,23 +1,30 @@
 package io.github.oldmanpushcart.test.qianfan4j;
 
-import io.github.oldmanpushcart.qianfan4j.chat.ChatModel;
-import io.github.oldmanpushcart.qianfan4j.chat.ChatRequest;
-import io.github.oldmanpushcart.qianfan4j.chat.message.Message;
-import io.github.oldmanpushcart.test.qianfan4j.chat.function.EchoFunction;
+import io.github.oldmanpushcart.qianfan4j.pluginapp.Plugin;
+import io.github.oldmanpushcart.qianfan4j.pluginapp.PluginAppModel;
+import io.github.oldmanpushcart.qianfan4j.pluginapp.PluginAppOptions;
+import io.github.oldmanpushcart.qianfan4j.pluginapp.PluginAppRequest;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+import java.net.URI;
+
+@Disabled
 public class DebugTestCase implements LoadingEnv {
 
     @Test
     public void test$debug() {
 
-        final var request = ChatRequest.newBuilder()
-                .model(ChatModel.ERNIE_V4)
-                .functions(new EchoFunction())
-                .messages(Message.ofUser("echo: HELLO WORLD!"))
+        final var request = PluginAppRequest.newBuilder()
+                .model(new PluginAppModel(PLUGIN_APP_ID))
+                .plugins(Plugin.KNOWLEDGE_BASE, Plugin.CHAT_OCR)
+                .question("请告诉我照片上有几辆自行车?")
+                .fileUri(URI.create("https://erniebot4j-image.bj.bcebos.com/image-002.jpeg"))
+                .option(PluginAppOptions.IS_STREAM, true)
+                .option(PluginAppOptions.IS_VERBOSE, true)
                 .build();
 
-        final var response = client.chat(request)
+        final var response = client.pluginApp(request)
                 .async()
                 .join();
 
